c32e73b223ad05fdfaed1ac9e7e4996358a84cac
ae <erradi@live.com>
2025-04-16 18:24:14 +0300

    Midterm

---
+++ midterm/index.js
@@ -0,0 +1,31 @@
+const API_URL =
+  "https://gist.githubusercontent.com/erradi/472379c776dc7d4ac7e0d4156404bc28/raw/f2f0a81d7f79e2976dcfc79858a83df9c2110e7e/crypto.json";
+
+/*
+
+// Home Page - coin-card
+<div class="coin-card">
+    <img src="https://via.placeholder.com/50" alt="Coin Icon">
+    <h2>Bitcoin</h2>
+    <p>Symbol: BTC</p>
+    <p>Price: $61,915.5</p>
+    <p>Market Cap: $1,121,393,739,784</p>
+    <button>Add to Wishlist</button>
+</div>
+
+
+// Wishlist Card
+<div class = "wishlist-card">
+<h2>Wishlist</h2>
+<a>baclk</a>
+<div class="coin-card">
+    <img src="https://via.placeholder.com/50" alt="Coin Icon">
+    <h2>Ethereum</h2>
+    <p>Symbol: ETH</p>
+    <p>Price: $3,895.7</p>
+    <p>Market Cap: $467,823,479,682</p>
+    <button>Remove</button>
+</div>
+</div>
+
+*/
